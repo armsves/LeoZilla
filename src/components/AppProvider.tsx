ef2f60a8c43b08a554127fabf426b723b7d357cc
Armando Medina <113925592+armsves@users.noreply.github.com>
2025-05-19 03:58:27 +0200

    trying to deploy

--- src/components/AppProvider.tsx
+++ src/components/AppProvider.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiConfig } from 'wagmi';
-import { wagmiConfig } from './EVMWalletConnect';
+//import { WagmiConfig } from 'wagmi';
+//import { wagmiConfig } from './EVMWalletConnect';
 import { AppContextProvider } from '@/app/context';
 
 const queryClient = new QueryClient();
@@ -10,11 +10,9 @@ const queryClient = new QueryClient();
 export function AppProvider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <WagmiConfig config={wagmiConfig}>
         <AppContextProvider>
           {children}
         </AppContextProvider>
-      </WagmiConfig>
     </QueryClientProvider>
   );
 } 
